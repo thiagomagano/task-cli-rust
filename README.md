c584f9e39b93dad084a27f030365855a3854b45d
Thiago Magano <thiagomagano1993@outlook.com>
2025-11-04 11:49:47 -0300

    initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# Task CLI - RUST
+
+Programa de linha de comando para treinar RUST.
+
+## Usage
+
+```console
+$ task <command> <argument>
+```
---
+++ src/main.rs
@@ -0,0 +1,9 @@
+// Programa para treinar CLI's in Rust
+fn main() {
+    //Pegar os argumentos passados para o programa 'argv[i]'
+    // task add <description>
+    // task list all / todo/ done/in-progress
+    //
+
+    println!("Hello, world!");
+}
