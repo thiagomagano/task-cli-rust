13de1f9fbd361d51dbfa15eb0622d04f25e3bb6e
Thiago Magano <thiagomagano1993@outlook.com>
2025-11-04 12:12:06 -0300

    função adicionar: lendo string e formatando mensagem

--- src/main.rs
+++ src/main.rs
@@ -5,9 +5,15 @@
 // task update <id> <description>
 // task mark-done <id>
 // task mark-in-progress <id>
-//
+
+fn adicionar(desc: &str) -> String {
+    format!("Tarefa \"{desc}\" adicionada com sucesso")
+}
+
 fn main() {
+    println!("====================== GERENCIE SUAS TAREFAS =============================\n\n\n\n");
     //Pegar os argumentos passados para o programa 'argv[i]'
 
-    println!("Hello, world!");
+    let task = adicionar(&String::from("Estudar Rust"));
+    println!("{task}");
 }
