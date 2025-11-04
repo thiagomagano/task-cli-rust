2d55e14b662de73c1469bf22476b0c2fb85ad858
Thiago Magano <thiagomagano1993@outlook.com>
2025-11-04 12:34:58 -0300

    lendo argumentos do programa e fazendo validaçoes iniciais

--- src/main.rs
+++ src/main.rs
@@ -6,14 +6,29 @@
 // task mark-done <id>
 // task mark-in-progress <id>
 
+use std::env;
+
 fn adicionar(desc: &str) -> String {
-    format!("Tarefa \"{desc}\" adicionada com sucesso")
+    format!("Tarefa \"{desc}\" adicionada com sucesso! ID: <id>")
 }
 
 fn main() {
-    println!("====================== GERENCIE SUAS TAREFAS =============================\n\n\n\n");
+    //println!("====================== GERENCIE SUAS TAREFAS =============================\n\n\n\n");
     //Pegar os argumentos passados para o programa 'argv[i]'
+    let args: Vec<String> = env::args().collect(); //
 
-    let task = adicionar(&String::from("Estudar Rust"));
-    println!("{task}");
+    //Verifica se tem a quantidade de argumentos necessária para rodar o programa
+    if args.len() < 3 {
+        println!("Uso: task <comando> <descrição>");
+        return;
+    }
+    //Lendo os argumentos passados acessando indices do vetor (notar o & para acessar valor de ponteiros)
+    let comando = &args[1];
+    let description = &args[2];
+
+    if comando == "add" {
+        println!("{}", adicionar(&description));
+    } else {
+        println!("Comando não reconhecido tente task <add> <description>")
+    }
 }
