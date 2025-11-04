afe97b3875b30d65d0bd309ba9cc8d5fe05c6623
Thiago Magano <thiagomagano1993@outlook.com>
2025-11-04 11:51:15 -0300

    batman

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,13 @@
 // Programa para treinar CLI's in Rust
+// task add <description>
+// task list <all/todo/done/in-progress>
+// task delete <id>
+// task update <id> <description>
+// task mark-done <id>
+// task mark-in-progress <id>
+//
 fn main() {
     //Pegar os argumentos passados para o programa 'argv[i]'
-    // task add <description>
-    // task list all / todo/ done/in-progress
-    //
 
     println!("Hello, world!");
 }
